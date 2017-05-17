b80821e8dc619591e06a6ed7f72bf99b7beb1797
Jason <mrjasonh98@gmail.com>
2017-05-17 22:18:37 +1000

    Moved JS to seperate file

---
+++ assets/scripts/menus.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#menu-toggle').click(function() {
+        $('.menu').slideToggle("fast");
+    });
+});
+
+
+$(document).ready(function() {
+    $('#sub-concerts-toggle').click(function() {
+        $('#sub-concerts').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#sub-concerts-mobile-toggle').click(function() {
+        $('#sub-concerts-mobile').slideToggle("fast");
+    });
+});
