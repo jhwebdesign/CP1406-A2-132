1341a5b875592051bb12751083b25463e99a6618
Jason <mrjasonh98@gmail.com>
2017-05-24 21:54:31 +1000

    Made dropdowns clickable/dynamic & other design changes
    Also added a download membership form banner at bottom of page.
    JS in seperate file.

---
+++ assets/scripts/history.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    $('#2004-year').click(function() {
+        $('#2004-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2006-year').click(function() {
+        $('#2006-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2007-year').click(function() {
+        $('#2007-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2008-year').click(function() {
+        $('#2008-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2009-year').click(function() {
+        $('#2009-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2010-year').click(function() {
+        $('#2010-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2011-year').click(function() {
+        $('#2011-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2012-year').click(function() {
+        $('#2012-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2013-year').click(function() {
+        $('#2013-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2014-year').click(function() {
+        $('#2014-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#2015-year').click(function() {
+        $('#2015-text').slideToggle("fast");
+    });
+});
