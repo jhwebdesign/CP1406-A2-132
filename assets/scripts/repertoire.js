835c179a5768bb2b3a79f71c6928cadbca948aa4
Jason <mrjasonh98@gmail.com>
2017-05-26 16:06:48 +1000

    Completed repertoire page
    Added content
    Added PDFs
    Included JS

---
+++ assets/scripts/repertoire.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('#rep1-year').click(function() {
+        $('#rep1-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#rep2-year').click(function() {
+        $('#rep2-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#rep3-year').click(function() {
+        $('#rep3-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#rep4-year').click(function() {
+        $('#rep4-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#rep5-year').click(function() {
+        $('#rep5-text').slideToggle("fast");
+    });
+});
+
+$(document).ready(function() {
+    $('#rep6-year').click(function() {
+        $('#rep6-text').slideToggle("fast");
+    });
+});
